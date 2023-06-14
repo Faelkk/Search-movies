d47889fe16e50cea2a4597e20b739a6448515443
= <achten.rafael16@gmail.com>
2023-06-14 00:07:06 -0300

    adicionando responsivo

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -8,7 +8,14 @@ import ButtonSearch from '../Components/Button';
 const Form = styled.form`
 display:flex;
 gap: 1rem;
-align-items: center
+align-items: center;
+
+@media (max-width: 768px) {
+   flex-direction:column
+    
+  }
+
+
 `
 
 const Label = styled.label`
@@ -17,15 +24,39 @@ font-family: Helvetica, Arial;
 @media (max-width: 768px) {
     font-size: 1rem;
   }
-`
+`;
 
 const Input = styled.input`
 border: 1px solid #eee;
 border-radius: 3px;
 background: #eee;
-padding: 5px
-
+padding: 5px;
 `
+
+
+const Button = styled.button`
+  padding: 10px;
+  background: #0c4369;
+  color: #fff;
+  font-weight: bold;
+  border-radius: 3px;
+  transition: .3s;
+  font-family: 'IBM Plex Mono', monospace;
+  letter-spacing: .1rem;
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    outline: none;
+    background: #1788cf;
+  }
+`;
+
+const ContainerInput = styled.div`
+display:flex;
+gap: 1rem;
+`
+
+
 
 const InputSearch = (props) => {
     const [searchValue, setSearch] = React.useState('');
@@ -39,20 +70,23 @@ const InputSearch = (props) => {
         window.localStorage.setItem('searchItem', searchValue)
     }
 
-    return <>
+    return (<>
         <Form className="FormSearch" onSubmit={handleSubmit}>
-            <Label htmlFor={props.id}>{props.label}</Label>
-            <Input
-                type={props.type}
-                id={props.id}
-                name={props.id}
-                autoComplete={props.autoComplete}
-                value={searchValue}
-                onChange={handleChange}
-            />
+            <ContainerInput>
+                <Label htmlFor={props.id}>{props.label}</Label>
+                <Input
+                    type={props.type}
+                    id={props.id}
+                    name={props.id}
+                    autoComplete={props.autoComplete}
+                    value={searchValue}
+                    onChange={handleChange}
+                />
+            </ContainerInput>
+            <Button onClick={handleSubmit}>Pesquisar</Button>
         </Form>
-        <ButtonSearch />
-    </>
+
+    </>)
 };
 
 export default InputSearch;
